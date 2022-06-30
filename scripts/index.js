38c9c3dcab694dcf2ec4ae96b713bb4c466ca4a5
Alexey Lavrinenkov <lavrinenkovalexey@yandex.ru>
2022-06-30 23:07:57 +0500

    For the second review

--- scripts/index.js
+++ scripts/index.js
@@ -7,36 +7,39 @@ const popupSubmit = document.querySelector('.popup__submit');
 const profileName = document.querySelector('.profile__name');
 const profileWork = document.querySelector('.profile__work');
 
-function togglePopup(popup) {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
 }
 
-profileEditButton.addEventListener('click', function (event) {
-  event.preventDefault();
-  document.body.style.overflow = 'hidden';
-  togglePopup(popup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopupAndFillForm() {
+  openPopup();
   popupInputName.value = profileName.textContent;
   popupInputWork.value = profileWork.textContent;
-});
+}
 
-popupCloseButton.addEventListener('click', function () {
-  togglePopup(popup);
-  document.body.style.overflow = 'visible';
-});
-
-popup.addEventListener('click', function(event) {
-  if (event.target == event.currentTarget) {
-    togglePopup(popup);
-    document.body.style.overflow = 'visible';
-  }
-});
-
-popupSubmit.addEventListener('click', function (event) {
+function closePopupAndFillProfile(event) {
   event.preventDefault();
-  togglePopup(popup);
+  closePopup();
   profileName.textContent = popupInputName.value;
   profileWork.textContent = popupInputWork.value;
-  document.body.style.overflow = 'visible';
+}
+
+profileEditButton.addEventListener('click', openPopupAndFillForm);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+/* Логика закрытия попапа для использования в будущем
+popup.addEventListener('click', function(event) {
+  if (event.target == event.currentTarget) {
+    closePopup(popup);
+  }
 });
+*/
+
+popupSubmit.addEventListener('click', closePopupAndFillProfile);
 
 
